832ddba89adc76c7f8e7d0d5cfe9373430feadfb
RomanFrei <roman.frei1@swisscom.com>
2012-12-03 10:06:02 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jumphost::Application.config.secret_token = '6804a4fde4eb79984371aee5d6257a05cccbc08a6ea9ff3b3f0b94e887ac070955f135770c8c339095ae6f8302e22b298a402239d8fdd443694df168a6105fa7'
